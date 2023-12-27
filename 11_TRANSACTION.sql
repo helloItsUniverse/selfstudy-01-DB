a24e62341be541ea5a3eabfca8fb62a275fd25cd
universe <hello.its.universe@gmail.com>
2023-12-27 12:47:49 +0900

    Transaction 수업 끝
    Transaction 수업 끝

--- 11_TRANSACTION.sql
+++ 11_TRANSACTION.sql
@@ -26,7 +26,7 @@ UPDATE tbl_menu
 
 SELECT * FROM tbl_menu; -- 이 시점에서 조회된 데이터는 실제 DB에 적용된 값이 아니다.
 -- 실제 작업은 JAVA 단에서 이루어짐.
-
+-- COMMIT;
 ROLLBACK;
 
 SELECT * FROM tbl_menu; -- ROLLBACK 또는 COMMIT 이후 SELECT한 것이 실제 DB에 적용된 값이다.
